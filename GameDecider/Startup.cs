dd2a10b9305936ac7aba8f341919200dbd7225c3
Adam Rodriguez <arodrigu@eecs.wsu.edu>
2016-03-15 07:50:03 -0700

    initial files

---
+++ GameDecider/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameDecider.Startup))]
+namespace GameDecider
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
